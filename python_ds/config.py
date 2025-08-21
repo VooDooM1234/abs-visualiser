42141216dc670a65c0d36765bb7f6bcb9ef6b853
voodoom <fake@email.com>
2025-08-21 14:43:44 +1000

    Updated config after removal

--- python_ds/config.py
+++ python_ds/config.py
@@ -1,14 +1,26 @@
 import os
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 def load_config():
     load_dotenv()
 
+    db_url = os.getenv("DATABASE_URL")
+
+    parsed = urlparse(db_url)
+
+    db_user = parsed.username
+    db_password = parsed.password
+    db_host = parsed.hostname
+    db_port = parsed.port
+    db_name = parsed.path.lstrip("/")  # removes leading "/"
+
     config = {
-        "DB_NAME": os.getenv("DATABASE_NAME"),
-        "DB_USER": os.getenv("DATABASE_USER"),
-        "DB_PASSWORD": os.getenv("DATABASE_PASSWORD"),
-        "DB_HOST": os.getenv("DATABASE_HOST"),
+        "DB_NAME": db_name,
+        "DB_USER": db_user,
+        "DB_PASSWORD": db_password,
+        "DB_HOST": db_host,
+        "DB_PORT": db_port,
     }
 
     if not all(config.values()):
